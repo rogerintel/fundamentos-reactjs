816b61978ced9901c9d5d74f1dcc7716113fed76
Rogério Carvalho <rogerintel@gmil.com>
2020-11-20 02:56:51 -0300

    Teste should be able to list the transactions sucess

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -63,7 +63,6 @@ const Dashboard: React.FC = () => {
               <img src={outcome} alt="Outcome" />
             </header>
             <h1 data-testid="balance-outcome">
-              R$
               {formatValue(Number(balance.outcome))}
             </h1>
           </Card>
@@ -73,7 +72,6 @@ const Dashboard: React.FC = () => {
               <img src={total} alt="Total" />
             </header>
             <h1 data-testid="balance-total">
-              R$
               {formatValue(Number(balance.total))}
             </h1>
           </Card>
@@ -91,18 +89,20 @@ const Dashboard: React.FC = () => {
             </thead>
 
             <tbody>
-              <tr>
-                <td className="title">Computer</td>
-                <td className="income">R$ 5.000,00</td>
-                <td>Sell</td>
-                <td>20/04/2020</td>
-              </tr>
-              <tr>
-                <td className="title">Website Hosting</td>
-                <td className="outcome">- R$ 1.000,00</td>
-                <td>Hosting</td>
-                <td>19/04/2020</td>
-              </tr>
+              {transactions &&
+                transactions.map(transaction => (
+                  <tr key={transaction.id}>
+                    <td className="title">{transaction.title}</td>
+                    <td className={transaction.type}>
+                      {transaction.type === 'outcome' ? '- ' : ''}
+                      {formatValue(transaction.value)}
+                    </td>
+                    <td>{transaction.category.title}</td>
+                    <td>
+                      {new Date(transaction.created_at).toLocaleDateString()}
+                    </td>
+                  </tr>
+                ))}
             </tbody>
           </table>
         </TableContainer>
